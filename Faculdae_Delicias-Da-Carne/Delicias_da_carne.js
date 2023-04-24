f63557f7c76d7e3a90aeec183c957de1055c36bf
lucca-software-infnet <123994038+lucca-software-infnet@users.noreply.github.com>
2023-04-24 14:47:47 -0300

    Projeto Delicias Da Carne

---
+++ Faculdae_Delicias-Da-Carne/Delicias_da_carne.js
@@ -0,0 +1,10 @@
+<!-- Incluindo o Bootstrap via CDN -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+
+<!-- Script para o carrossel -->
+<script>
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
+</script>
